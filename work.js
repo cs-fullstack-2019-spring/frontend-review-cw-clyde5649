db0f1b8db9ba647a66bd56d907b7d86b510e8972
clyde5649 <clyde.bledsoe@yahoo.com>
2019-02-05 11:54:31 -0600

    work

---
+++ work.js
@@ -0,0 +1,19 @@
+console.log("test")
+
+
+function clickme(i) {
+
+}
+
+
+function clickthenumber(e) {
+
+}
+userinput1 = n
+userput = ""
+while (userinput1 == userinput1);
+console.log("stop")
+
+else (userinput1 == userput)
+console.log(anything else)
+
